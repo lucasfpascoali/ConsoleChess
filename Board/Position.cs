e5646d36e8615015538ee54242c3007e36adff3e
Lucas Furlanetto Pascoali <luccafpascoali@gmail.com>
2023-06-21 18:19:39 -0300

    Create Position Class

---
+++ Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess.Board
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public override string ToString()
+        {
+            return $"{Row}, {Col}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Chess
+﻿using Chess.Board;
+
+namespace Chess
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position p = new(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
